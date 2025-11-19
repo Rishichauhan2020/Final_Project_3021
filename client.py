4f41ce52125a3fec6ab63975be70baa12071d3b1
rishi kumar <29rishi2004@gmail.com>
2025-11-19 14:15:36 -0600

    Updated files.

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ from urllib.parse import urlparse
 HOST = '127.0.0.1'
 PORT = 5000
 
-filename = input("Enter the filename to download: ") 
+filename = input("Enter the filename to download here: ") 
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
@@ -17,6 +17,7 @@ data = client.recv(409600)
 with open(filename, 'wb') as f:
     f.write(data)
 
+## 
 def get_data():
     url = 'https://insecure-api.com/get-data'
     
